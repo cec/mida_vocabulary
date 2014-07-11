539b559bf5dcc4dcd376357e77ff4b7a64ba76be
Francesco Rigotti <francesco.rigotti@luganonetwork.ch>
2014-07-11 13:34:53 +0200

    working on fix

--- lib/mida_vocabulary.rb
+++ lib/mida_vocabulary.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift File.dirname(__FILE__)
-require File.dirname(__FILE__) + '/mida_vocabulary/propertydesc.rb'
-Dir.glob(File.dirname(__FILE__) + '/mida_vocabulary/*.rb').reverse {|file| require file}
+# require File.dirname(__FILE__) + '/mida_vocabulary/propertydesc.rb'
+# require File.dirname(__FILE__) + '/mida_vocabulary/propertydesc.rb'
+# require File.dirname(__FILE__) + '/mida_vocabulary/propertydesc.rb'
+Dir.glob(File.dirname(__FILE__) + '/mida_vocabulary/**/*.rb').reverse {|file| require file}
 
 
 # Mida is a Microdata parser and extractor.
